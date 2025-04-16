a6b8d3ad0463a669c2f555336b09b9cf3860b150
g-i-kala <5star@wp.pl>
2025-04-16 14:03:51 +0200

    feat:  Notes display on logged in conditions

--- app/Views/partials/nav.php
+++ app/Views/partials/nav.php
@@ -44,10 +44,12 @@
                         <a href="/about"
                             class="<?= isUrl('/about') ? 'bg-gray-900' : 'bg-gray-800' ; ?> rounded-md px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
                             aria-current="page">About</a>
-                        <a href="/notes"
+                        <?php if ($_SESSION['user'] ?? false) : ?>
+                            <a href="/notes"
                             class="<?= isUrl('/contact') ? 'bg-gray-900' : 'bg-gray-800' ; ?> rounded-md  px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
                             aria-current="page">Notes</a>
-                        <a href="/contact"
+                        <?php endif; ?>
+                            <a href="/contact"
                             class="<?= isUrl('/contact') ? 'bg-gray-900' : 'bg-gray-800' ; ?> rounded-md  px-3 py-2 text-sm font-medium text-white hover:bg-gray-700"
                             aria-current="page">Contact</a>
                     </div>
